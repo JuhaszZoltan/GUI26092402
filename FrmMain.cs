856df60765d2ebae9f29c3ab6a5336574df3ec04
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-09-24 12:28:07 +0200

    updt

--- FrmMain.cs
+++ FrmMain.cs
@@ -1,33 +1,28 @@
-namespace GUI26092402
+namespace GUI26092402;
+
+#nullable disable
+
+public partial class FrmMain : Form
 {
-    public partial class FrmMain : Form
+    public FrmMain()
     {
-        public FrmMain()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            lblCornflowerBlue.Click += LblColoring_Click;
-            lblGold.Click += LblColoring_Click;
-            lblHotPink.Click += LblColoring_Click;
-            lblSpringGreen.Click += LblColoring_Click;
+        lblCornflowerBlue.Click += LblColoring_Click;
+        lblGold.Click += LblColoring_Click;
+        lblHotPink.Click += LblColoring_Click;
+        lblSpringGreen.Click += LblColoring_Click;
 
-            btnExit.Click += BtnExit_Click;
-            btnReset.Click += BtnReset_Click;
-        }
-
-        private void BtnReset_Click(object? sender, EventArgs e)
-        {
-            this.BackColor = Color.White;
-        }
-
-        private void BtnExit_Click(object? sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void LblColoring_Click(object? sender, EventArgs e)
-        {
-            this.BackColor = (sender as Label).BackColor;
-        }
+        btnExit.Click += BtnExit_Click;
+        btnReset.Click += BtnReset_Click;
     }
+
+    private void BtnReset_Click(object sender, EventArgs e) =>
+        this.BackColor = Color.White;
+
+    private void BtnExit_Click(object sender, EventArgs e) => 
+        Application.Exit();
+
+    private void LblColoring_Click(object sender, EventArgs e) =>
+        this.BackColor = (sender as Label).BackColor;
 }
